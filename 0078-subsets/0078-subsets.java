05d798995a3a9cb786bdf41105d526cf1079f57e
Shubham Chaudhary <125564143+shubham241201@users.noreply.github.com>
2023-10-10 15:56:14 +0530

    Time: 1 ms (72.05%), Space: 41.8 MB (59.08%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,26 +2,27 @@ class Solution {
 public List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> list = new ArrayList<>();
     Arrays.sort(nums);
-    helper(list, new ArrayList<>(), nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
     return list;
 }
 
-private static void helper(List<List<Integer>> list, List<Integer> temp, int[] nums){
-    if(nums.length == 0){
-        //checking if temp we're gonna add is a duplicate or not
-        for(List<Integer> oh : list){
-            if(temp.equals(oh)) return;
-        }    
-        list.add(new ArrayList<>(temp));
+private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+    // list.add(new ArrayList<>(tempList));
+    // for(int i = start; i < nums.length; i++){
+    //     tempList.add(nums[i]);
+    //     backtrack(list, tempList, nums, i + 1);
+    //     tempList.remove(tempList.size() - 1);
+    // }
+
+    if(start >= nums.length){
+        list.add(new ArrayList<>(tempList));
         return;
-        
     }
 
-    temp.add(nums[0]);
-    helper(list, temp, Arrays.copyOfRange(nums, 1, nums.length));
-    temp.remove(temp.size()-1);
-    helper(list, temp, Arrays.copyOfRange(nums, 1, nums.length));
-}
+    tempList.add(nums[start]);
+    backtrack(list, tempList, nums, start+1);
+    tempList.remove(tempList.size()-1);
+    backtrack(list, tempList, nums, start+1);
 }    
-
+}
     
