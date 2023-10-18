04711ab69985f37dd9ca174ababa4257d38a03b8
Shubham Chaudhary <125564143+shubham241201@users.noreply.github.com>
2023-10-18 21:13:13 +0530

    Time: 11 ms (40.64%), Space: 55.7 MB (55.67%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,61 +1,53 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-
-        ListNode mid = getMid(head);
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-
-        return merge(left, right);
+public class Solution {
+  
+  public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+        
+    // step 1. cut the list to two halves
+    ListNode prev = null, slow = head, fast = head;
+    
+    while (fast != null && fast.next != null) {
+      prev = slow;
+      slow = slow.next;
+      fast = fast.next.next;
     }
+    
+    prev.next = null;
+    
+    // step 2. sort each half
+    ListNode l1 = sortList(head);
+    ListNode l2 = sortList(slow);
+    
+    // step 3. merge l1 and l2
+    return merge(l1, l2);
+  }
+  
+  ListNode merge(ListNode l1, ListNode l2) {
+      ListNode head = new ListNode();
+      ListNode temp = head;
 
-    public ListNode merge(ListNode head1, ListNode head2) {
-        ListNode newNode = new ListNode();
-        ListNode temp = newNode;
-        while(head1 != null && head2 != null){
-            if(head1.val > head2.val){
-                temp.next = head2;
-                head2 = head2.next;
-                temp = temp.next;
-            }else{
-                temp.next = head1;
-                head1 = head1.next;
-                temp = temp.next;
-            }
-        }
-        while(head1 != null){
-            temp.next = head1;
-            head1 = head1.next;
+      while(l1!= null && l2!= null){
+          if(l1.val > l2.val){
+              temp.next = l2;
+              l2 = l2.next;
+              temp = temp.next;
+          }else{
+              temp.next = l1;
+              l1 = l1.next;
+              temp = temp.next;
+          }
+      }
+      while(l1 != null){
+            temp.next = l1;
+            l1 = l1.next;
             temp = temp.next;
-        }
-        while(head2 != null){
-            temp.next = head2;
-            head2 = head2.next;
-            temp = temp.next;
-        }
-        return newNode.next;
-    }
-
-    ListNode getMid(ListNode head){
-        ListNode midPrev = null;
-        while (head != null && head.next != null) {
-            midPrev = (midPrev == null) ? head : midPrev.next;
-            head = head.next.next;
-        }
-        ListNode mid = midPrev.next;
-        midPrev.next = null;
-        return mid;
-    }
+      }
+      while(l2 != null){
+          temp.next = l2;
+          l2 = l2.next;
+          temp = temp.next;
+      }
+      return head.next;
+  }
 }
