46b283e3b1586772b90db905c64766e20b2c69f1
Shubham Chaudhary <125564143+shubham241201@users.noreply.github.com>
2023-10-18 14:26:18 +0530

    Time: 0 ms (100%), Space: 43.1 MB (98.05%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,43 +11,26 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        int length = countNodesinLoop(head);
         ListNode f = head;
         ListNode s = head;
-        if(length == 0){
-            return null;
+        if(head == null){
+            return head;
         }
-        while(length > 0){
+        while(f!= null && f.next != null){
+            f = f.next.next;
             s = s.next;
-            length--;
+            if(f == s){
+                s = head;
+                while(f!= s){
+                f = f.next;
+                s = s.next;
+                }
+                return s;
+            }
         }
-        while(f!=s){
-            f = f.next;
-            s = s.next;
-        }
-        return s;
+        
+        return null;
     }
 
-    public int countNodesinLoop(ListNode head)
-    {
-        ListNode fast = head;
-        ListNode slow = head;
-        while(fast!= null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-            if(fast == slow){
-                int length = 0;
-                ListNode temp = slow;
-                do{
-                    temp = temp.next;
-                    length++;
-                }while(temp!=slow);
-                return length;
-            }   
-        }
-        return 0;
-    }
     
-
-
 }
