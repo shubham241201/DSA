42748f125d71cba0d322eca523628ef87793482d
Shubham Chaudhary <125564143+shubham241201@users.noreply.github.com>
2023-10-19 17:07:56 +0530

    Time: 9 ms (89.44%), Space: 56.9 MB (7.04%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,53 +1,56 @@
 public class Solution {
   
   public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
-        
-    // step 1. cut the list to two halves
-    ListNode prev = null, slow = head, fast = head;
-    
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-    
-    prev.next = null;
-    
-    // step 2. sort each half
-    ListNode l1 = sortList(head);
-    ListNode l2 = sortList(slow);
-    
-    // step 3. merge l1 and l2
-    return merge(l1, l2);
-  }
-  
-  ListNode merge(ListNode l1, ListNode l2) {
+      if(head == null || head.next == null){
+          return head;
+      }
+
+      ListNode f = head;
+      ListNode s = head;
+      ListNode prev = null;
+      
+      while(f != null && f.next !=null){
+          prev = s;
+          s = s.next;
+          f = f.next.next; 
+      }
+
+      prev.next = null;
+
+      ListNode l1 = sortList(head);
+      ListNode l2 = sortList(s);
+
+      return merge(l1, l2);
+  }  
+
+
+  ListNode merge(ListNode l1, ListNode l2){
       ListNode head = new ListNode();
       ListNode temp = head;
 
-      while(l1!= null && l2!= null){
+      while(l1 != null && l2 != null){
           if(l1.val > l2.val){
               temp.next = l2;
               l2 = l2.next;
-              temp = temp.next;
+            //   temp = temp.next;
           }else{
               temp.next = l1;
               l1 = l1.next;
-              temp = temp.next;
+            //   temp = temp.next;
           }
-      }
-      while(l1 != null){
-            temp.next = l1;
-            l1 = l1.next;
-            temp = temp.next;
-      }
-      while(l2 != null){
-          temp.next = l2;
-          l2 = l2.next;
           temp = temp.next;
       }
-      return head.next;
+
+      if(l1 != null){
+              temp.next = l1;
+              temp = temp.next;
+       }
+
+        if(l2 != null){
+            temp.next = l2;
+            temp = temp.next;
+        }
+          
+        return head.next;   
   }
 }
